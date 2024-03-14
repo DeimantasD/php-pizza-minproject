3935b2341e10015431400f5be1ff84c4e86547ed
DeimantasD <deimantas.damkus@gmail.com>
2024-03-14 12:16:12 +0200

    final

--- templates/header.php
+++ templates/header.php
@@ -1,9 +1,8 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Ninja Pizza</title>
+  <title>GiT Pizza</title>
   
-
   <link rel="stylesheet" href="styles/style.css">
 
 </head>
